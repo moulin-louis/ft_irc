cf13ca06040da2949441d5f21a25bc927d6719dc
Louis Moulin <loumouli@e3r2p23.clusters.42paris.fr>
2023-03-21 15:04:43 +0100

    delete old client

---
+++ inc/irc.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   irc.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: loumouli <loumouli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/21 12:12:48 by loumouli          #+#    #+#             */
+/*   Updated: 2023/03/21 14:31:43 by loumouli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef IRC_HPP
+#define IRC_HPP
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <cerrno>
+#include <csignal>
+#include <cstdio>
+
+typedef int Socket;
+
+#define INVALID_SOCKET -1
+#define SOCKET_ERROR -1
+
+using namespace std;
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,54 @@
 #include <iostream>
+#include "irc.hpp"
 
-int main( void ) {
-	std::cout << "test" << std::endl;
-	return 1;
+int main( int ac, char **av ) {
+	if ( ac != 3 ) {
+		cout << "usage is : ./ircserv <port> <password>";
+		return EXIT_FAILURE;
+	}
+	int port = atoi(av[1]);
+	Socket sock = socket( AF_INET, SOCK_STREAM  , 0);
+	if (sock == -1) {
+		cout << "socket creatin: " << strerror(errno) << endl;
+		return EXIT_FAILURE;
+	}
+
+	cout << "Socket created" << endl;
+
+	sockaddr_in	sin;
+	sin.sin_addr.s_addr = htonl(INADDR_ANY);
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(port);
+
+	if (bind(sock, (sockaddr *)&sin, sizeof(sin)) == -1 ) {
+		perror("bind: ");
+		close(sock);
+		return EXIT_FAILURE;
+	}
+
+	cout << "bind done" << endl;
+
+	if (listen(sock, 4096) == -1 ) {
+		perror("listen: ");
+		close(sock);
+		return EXIT_FAILURE;
+	}
+
+	cout << "socket is listening.." << endl;
+
+	sockaddr_in csin;
+	socklen_t crecsize = sizeof(csin);
+	Socket csock = accept(sock, (sockaddr*)&csin, &crecsize);
+
+	if (csock == -1) {
+		perror("accept: ");
+		close(sock);
+		return EXIT_FAILURE;
+	}
+
+	cout << "connection accepted" << endl;
+
+	close(sock);
+	close(csock);
+	return EXIT_SUCCESS;
 }
