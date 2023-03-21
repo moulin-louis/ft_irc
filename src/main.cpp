f113280cc7236afd371279092286a0547fdf02fa
Louis Moulin <loumouli@e3r2p23.clusters.42paris.fr>
2023-03-21 16:16:15 +0100

    add simple server/client

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ int main( int ac, char **av ) {
 		return EXIT_FAILURE;
 	}
 	int port = atoi(av[1]);
+	
 	Socket sock = socket( AF_INET, SOCK_STREAM  , 0);
 	if (sock == -1) {
 		cout << "socket creatin: " << strerror(errno) << endl;
@@ -39,15 +40,18 @@ int main( int ac, char **av ) {
 	sockaddr_in csin;
 	socklen_t crecsize = sizeof(csin);
 	Socket csock = accept(sock, (sockaddr*)&csin, &crecsize);
-
 	if (csock == -1) {
 		perror("accept: ");
 		close(sock);
 		return EXIT_FAILURE;
 	}
-
 	cout << "connection accepted" << endl;
-
+	if (send(csock, "Hello World\n", strlen("Hello Word") + 1, 0) == -1) {
+		perror("send: ");
+		close(sock);		
+		close(csock);
+		return EXIT_FAILURE;
+	}
 	close(sock);
 	close(csock);
 	return EXIT_SUCCESS;
