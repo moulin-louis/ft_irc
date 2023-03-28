77e1bfe152aa9ff05e962a04781fe3b83a2e4bd6
tnoulens <thibault.noulens@proton.me>
2023-03-28 16:16:34 +0200

    modif 001

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -27,8 +27,8 @@ static int epoll_ctl_add(int epfd, int fd, uint32_t events) {
 	return (epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &ev));
 }
 
-void	sendMessage(Client &client, const std::string& message) {
-	send(client.getFd(), message.c_str(), message.length(), 0);
+int	sendMessage(Client &client, const std::string& message) {
+	return (send(client.getFd(), message.c_str(), message.length(), 0));
 }
 
 /*-------------------------------CONSTRUCTORS---------------------------------*/
@@ -196,8 +196,8 @@ void	Server::process_input(Socket fd ) {
 	}
 	temp.resize(byte_count);
 	cout << YELLOW << byte_count << " bytes received" << RESET << endl;
-	cout << PURPLE << "cmd = " << temp << RESET << endl;
-	while (1) {
+	cout << CYAN << "cmd = " << temp << RESET << endl;
+	while (true) {
 		if (temp.find(endmsg) == string::npos)
 			break ;
 		string tok = temp.substr(0, temp.find(endmsg));
@@ -205,17 +205,13 @@ void	Server::process_input(Socket fd ) {
 		temp.erase(0, temp.find(endmsg) + 2);
 	}
 	parse_command((const string)client.getBuff(), client);
-	//	if (client.getBuff()[client.getBuff().length() - 1] == '\n') {
-//		std::cout << "Command received from : " << client.getHostname() << std::endl;
-//		std::cout << CYAN << client.getBuff() << RESET;
-//	}
-	byte_count = send(client.getFd(), client.getBuff().c_str(), client.getBuff().length(), 0);
+	byte_count = sendMessage(client, client.getBuff());
 	if (byte_count == -1) {
 		throw runtime_error(string("send: ") + strerror(errno));
 	}
 	cout << YELLOW << byte_count << " bytes sent" << RESET << endl;
 	client.clearBuff();
-	cout << "buff is now" << client.getBuff() << endl;
+	cout << "buff is now cleared" << client.getBuff() << endl;
 }
 
 void	Server::parse_command(basic_string<char> input, Client& client ) {
