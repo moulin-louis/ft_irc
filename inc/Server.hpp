7adb59702078b6ffe30b67eaf224b179aa9ab3f5
Louis Moulin <loumouli@e3r2p23.clusters.42paris.fr>
2023-03-29 15:38:58 +0200

    fix main branch

--- inc/Server.hpp
+++ inc/Server.hpp
@@ -28,7 +28,7 @@
 class Channel;
 
 typedef void (*command_function)( vector<string>, Client& );
-ssize_t 	sendMessage(Client &client, const std::string& message);
+ssize_t 	sendMessage(Client &, const string& );
 typedef std::map<Socket, Client>::iterator	client_iter;
 typedef vector<Channel>::iterator chan_iter;
 typedef vector<Socket>::iterator cl_iter;
@@ -52,44 +52,45 @@ class Server
 		map<string, command_function>	cmd_map;
 		vector<Channel>					chan_vec;
 
-		Server(const char *port, const string &password);
+		Server(const char *, const string &);
 		~Server();
 
 		//request from client
 		void	process_input( Socket );
 
 		//respond from server
-		void	add_rply_from_server(const string&  msg, Client& dest, const string&  cmd, int code);
+		void	add_rply_from_server(const string& , Client& , const string& , int );
 
 		//checks
-		Client&		find_user(const string&  nick, Client client);
-		Channel&	find_channel(const string&  name, Client client);
-		void		is_valid_nickname(string &nickname, Client& client);
-		void		is_valid_username(string &username, Client& client);
-		void		is_valid_chan_name(vector<string> params, Client& client);
+		Client&		find_user(const string& , Client );
+		Channel&	find_channel(const string& , Client );
+		void		is_valid_nickname(string &, Client& );
+		void		is_valid_username(string &, Client& );
+		void		is_valid_chan_name(vector<string> , Client& );
 
 		//commands
-		void	parse_command(basic_string<char> input, Client& client );
-		void	nick(vector<string> params, Client& client);
-		void	pass(vector<string> params, Client& client);
-		void	ping(vector<string> params, Client& client);
-		void	user(vector<string> params, Client& client);
-		void	join(vector<string> params, Client& client);
-		void	private_msg(vector<string> params, Client&);
-		void	oper( vector<string> params, Client& client);
-		void	quit( vector<string> params, Client& client);
-		void	mode( vector<string> params, Client& client );
-		void	topic( vector<string> params, Client& client );
+		void	parse_command(basic_string<char> , Client&  );
+		void	nick(vector<string> , Client& );
+		void	pass(vector<string> , Client& );
+		void	ping(vector<string> , Client& );
+		void	user(vector<string> , Client& );
+		void	join(vector<string> , Client& );
+		void	private_msg(vector<string> , Client&);
+		void	oper( vector<string> , Client& );
+		void	quit( vector<string> , Client& );
+		void	mode( vector<string> , Client& );
+		void	topic( vector<string> , Client& );
+		void	kill( vector<string> , Client& );
 
-		void    process_topic_cmd(vector <string> params, Client& client, Channel& chan);
+		void    process_topic_cmd(vector <string> , Client& , Channel& );
 
 		//server run functions
 		void 	run();
 
 	//send messages
-	void	notify_chan(const string& chan, const string& content, const string& cmd, Client &client);
-	void	add_cmd_client(const string& content, Client& client, Client&, const string&  cmd);
-	void	add_cmd_client(const string&, Client&, Client&, const string&  cmd, Channel& chan);
+	void	notify_chan(const string& , const string& , const string& , Client &);
+	void	add_cmd_client(const string& , Client& , Client&, const string&  );
+	void	add_cmd_client(const string&, Client&, Client&, const string& , Channel& );
 
 };
 
