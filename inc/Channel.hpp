e77f0e64ff695e1552471d75d3399d9051da5483
Louis Moulin <loumouli@e3r2p23.clusters.42paris.fr>
2023-03-29 15:40:21 +0200

    fix types include

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -28,24 +28,24 @@ class Channel
         string _topic;
     public:
         Channel();
-        Channel(string &name, Client& client);
-        Channel(const Channel &src);
+        Channel(string &, Client& );
+        Channel(const Channel &);
         ~Channel();
-        Channel &operator=(const Channel &rhs);
+        Channel &operator=(const Channel &);
 
 		//vector of client's fd
         vector<Socket> clients;
 
         const string    &getName() const;
         const string    &getTopic() const;
-        void            setTopic(string &topic);
-        void            setName(string &name);
+        void            setTopic(string &);
+        void            setName(string &);
 
     //checks
-        bool	user_in_chan(Client& client);
+        bool	user_in_chan(Client& );
 
     //actions
-        void    addClient(Client &client);
+        void    addClient(Client &);
 };
 
 #endif
