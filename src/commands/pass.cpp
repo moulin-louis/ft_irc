169cdb81d60c0984bf563061a17715caa9dd9064
loumouli <moulin-louis@louis.moulin@outlook.fr>
2023-03-30 10:27:18 +0200

    fix merge

--- src/commands/pass.cpp
+++ src/commands/pass.cpp
@@ -19,7 +19,7 @@ void	Server::pass(vector<string> params, Client& client) {
 			add_rply_from_server(":Not enough parameters", client, "PASS", ERR_NEEDMOREPARAMS);
 			throw invalid_argument("pass: Not enough parameters");
 		}
-		if ( !client.passwd_provided ) {
+		if ( client.passwd_provided ) {
 			add_rply_from_server(":Unauthorized command (already registered)", client, "PASS", ERR_ALREADYREGISTRED);
 			throw invalid_argument("pass: Unauthorized command (already registered)");
 		}
