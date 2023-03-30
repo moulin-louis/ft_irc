877155875851eb9f02f2beb1a8cc6136d5369567
Waxxy <72367316@protonmail.com>
2023-03-30 18:46:34 +0200

    list ok

--- src/commands/list.cpp
+++ src/commands/list.cpp
@@ -3,18 +3,54 @@
 /*                                                        :::      ::::::::   */
 /*   list.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: loumouli <loumouli@student.42.fr>          +#+  +:+       +#+        */
+/*   By: waxxy <waxxy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 15:49:09 by tnoulens          #+#    #+#             */
-/*   Updated: 2023/03/29 16:34:58 by loumouli         ###   ########.fr       */
+/*   Updated: 2023/03/30 13:49:19 by waxxy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
 
+static void displayAllChannels(Server &server, Client &client, vector<Channel> &chan_vec)
+{
+	for (chan_iter it = chan_vec.begin(); it != chan_vec.end(); it++)
+		server.add_rply_from_server(":" + it->getName() + " " + itostr(it->clients.size()) + " " + it->getTopic(), client, "LIST", RPL_LIST);
+	server.add_rply_from_server(":End of LIST", client, "LIST", RPL_LISTEND);
+}
+
+static void	getSpecifiedChannels(Server &server, Client &client, vector<Channel> &chan_vec, vector<std::string> &params)
+{
+	std::istringstream	ss(params[0]);
+	std::string 		chan_name;
+
+	while (std::getline(ss, chan_name, ','))
+	{
+		if (chan_name[0] != '#')
+			continue;
+		else
+			for (chan_iter it = chan_vec.begin(); it != chan_vec.end(); it++)
+				if (it->getName() == chan_name)
+					server.add_rply_from_server(":" + it->getName() + " " + itostr(it->clients.size()) + " " + it->getTopic(), client, "LIST", RPL_LIST);
+	}
+	server.add_rply_from_server(":End of LIST", client, "LIST", RPL_LISTEND);
+}
+
 void	Server::list(vector<string> params, Client &client)
 {
-	(void)params;
-	(void)client;
-	cout << this->chan_vec[0].getName() << endl;
+	if (params.empty())
+		throw invalid_argument("list: invalid number of parameters");
+	if (!params[0].length())
+		::displayAllChannels(*this, client, this->chan_vec);
+	else if (params[0].length() && params.size() == 1)
+		::getSpecifiedChannels(*this, client, this->chan_vec, params);
+	else
+	{
+		//Theoretically, we should check on the server netwok if the server exists,
+		// but here, we only check if the specified server name is the same as _server_name
+		if (params[1] != this->_server_name)
+			add_rply_from_server(":No such server", client, "LIST", ERR_NOSUCHSERVER);
+		else
+			::getSpecifiedChannels(*this, client, this->chan_vec, params);
+	}
 }
