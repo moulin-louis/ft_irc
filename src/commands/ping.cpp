549c239baef5127187a52f16aae3e827f34df456
Waxxy <72367316@protonmail.com>
2023-04-06 09:14:10 +0200

    ping

--- src/commands/ping.cpp
+++ src/commands/ping.cpp
@@ -15,11 +15,11 @@
 void Server::ping(vector<string>& params, Client &client) {
 	try {
 		if ( params.empty() ) {
-			add_rply_from_server(":No origin specified", client, "PING", ERR_NOORIGIN);
+			add_rply_from_server(":No origin specified", client, "PING ", ERR_NOORIGIN);
 			throw invalid_argument("ping: No origin specified");
 		}
 		if ( params[0] != this->_server_name ) {
-			add_rply_from_server(params[0] + " :No such server", client, "PING", ERR_NOSUCHSERVER);
+			add_rply_from_server(params[0] + " :No such server", client, "PING ", ERR_NOSUCHSERVER);
 			throw invalid_argument("ping: No such server");
 		}
 		client.setBuff("PONG " + params[0] + endmsg);
