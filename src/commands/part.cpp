2fd32b66ede025153ee2b4201276508c5a3765cf
waxxy <tnoulens@student.fr>
2023-04-11 14:38:47 +0200

    fix part

--- src/commands/part.cpp
+++ src/commands/part.cpp
@@ -56,34 +56,39 @@ void	Server::process_part_cmd(Channel& chan, Client& client, string& reason)
 
 void	Server::part(vector<string>& params, Client& client)
 {
-	try {
+	try
+    {
 		if (params.empty())
 		{
 			this->add_rply_from_server(":Not enough parameters", client, "PART", ERR_NEEDMOREPARAMS);
 			throw  invalid_argument("part: Not enough parameters");
 		}
 		vector<string>	chans_to_part;
-		size_t			pos;
-		string delimiter = ",";
 //		irssi always appends the channel of the active channel window as a parameter
 //		if the active window is the main window, and no parameter is given then an error
 //		is thrown by irssi, we push back it directly to the vector as it already has the #
 		if (params.size() == 1)
 			chans_to_part.push_back(params[0]);
 //		if there are parameters given then we get these channels to part instead of the active one:
-		else if (params.size() > 1)
+		else
 		{
+            size_t			pos;
+            string delimiter = ",";
 			params[1].erase(0, 1);
-			while ((pos = params[1].find(delimiter)) != string::npos) {
+			while ((pos = params[1].find(delimiter)) != string::npos)
+            {
 				chans_to_part.push_back("#" + params[1].substr(0, pos));
-				params[1].erase(1, pos + delimiter.length());
+				params[1].erase(0, pos + delimiter.length());
 			}
 			chans_to_part.push_back("#" + params[1]);
 		}
-		for ( str_iter it = chans_to_part.begin(); it != chans_to_part.end(); it++) {
+		for ( str_iter it = chans_to_part.begin(); it != chans_to_part.end(); it++)
+        {
 			bool chan_exists = false;
-			for (chan_iter it2 = this->chan_vec.begin(); it2 != this->chan_vec.end(); it2++) {
-				if (it2->getName() == *it) {
+			for (chan_iter it2 = this->chan_vec.begin(); it2 != this->chan_vec.end(); it2++)
+            {
+				if (it2->getName() == *it)
+                {
 					if (params.size() > 2)
 					{
 						string reason = params[2];
@@ -101,7 +106,8 @@ void	Server::part(vector<string>& params, Client& client)
 				this->add_rply_from_server(" :" + *it + " :No such channel", client, "PART", ERR_NOSUCHCHANNEL);
 		}
 	}
-	catch ( exception& x ) {
+	catch ( exception& x )
+    {
 		cout << RED << x.what() << RESET << endl;
 	}
 }
