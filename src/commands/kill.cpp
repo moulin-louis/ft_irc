a2afe9a27414206fee935827e9abba1d3782dbfa
Mael PIGNET <you@example.com>
2023-04-24 16:27:42 +0200

    kill rectif

--- src/commands/kill.cpp
+++ src/commands/kill.cpp
@@ -6,16 +6,15 @@
 /*   By: mpignet <mpignet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 16:33:26 by mpignet           #+#    #+#             */
-/*   Updated: 2023/04/24 15:29:03 by mpignet          ###   ########.fr       */
+/*   Updated: 2023/04/24 16:22:53 by mpignet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
 
 void	Server::kill( const vector<string>& params, Client &client) {
-	cout << "kill" << endl;
 	try {
-		if ( params.size() != 2 ) {
+		if ( params.size() < 2 ) {
 			add_rply_from_server(" :Not enough parameters", client, "KILL", ERR_NEEDMOREPARAMS);
 			throw invalid_argument("kill: Not enough parameters");
 		}
@@ -30,10 +29,21 @@ void	Server::kill( const vector<string>& params, Client &client) {
 	}
 	for ( client_iter it = this->fd_map.begin(); it != this->fd_map.end(); ++it) {
 		if (it->second.getNickname() == params[0]) {
-			add_rply_from_server(params[1], client, "", 0);
-			sendMessage(client, client.getBuff());
-			this->_disconect_client(it->second.getFd());
-			throw runtime_error("client lost connection");
+			vector<string> reason;
+			if (params.size() > 2) {
+				reason = params;
+				reason.erase(reason.begin(), reason.begin() + 1);
+				quit(reason, it->second);
+				this->_disconect_client(it->first);
+			}
+			else {
+				reason.push_back(":leaving");
+				add_cmd_client("leaving", it->second, client, "KILL");
+				quit(reason, it->second);
+				this->_disconect_client(it->first);
+			}
+			
+			return ;
 		}
 	}
 	add_rply_from_server(" :" + params[0] + " :No such nick/channel", client, "KILL", ERR_NOSUCHNICK);
